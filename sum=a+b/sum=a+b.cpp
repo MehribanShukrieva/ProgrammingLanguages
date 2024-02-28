c8f3c95b72d459defa5059723b71e73f9fd235bf
Mehriban Shukrieva <127889525+marynotfound@users.noreply.github.com>
2024-02-28 13:56:33 +0300

    Sum=a+b.cpp

--- sum=a+b/sum=a+b.cpp
+++ sum=a+b/sum=a+b.cpp
@@ -1,12 +1,12 @@
 #include<stdio.h>
  int main() {
 	int a,b,sum;
-	
+	// Take two numbers from the keyboard
 	printf("Enter two numbers:");
 	scanf("%d %d", &a,&b);
 	
 	sum=a+b;
-	
+	// Display the sum of these two numbers
 	printf("sum: %d",sum);
 	return 0;
 }
